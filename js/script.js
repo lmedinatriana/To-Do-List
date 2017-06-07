9832d77cd132fbf48ec880a93b39b7743d0cff91
ACE User <aceuser@e140-s17.lab.lagcc>
2017-06-07 19:18:35 -0400

    Performed operators and if statements and if, else, and if else statements

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//This crates the global variable x and assigns it the number value of 5
+//These are 3 different ways to increment the value of x by 1
+// var x = 5;
+// console.log(x);
+// x += 1;
+// console.log(x);
+// x++;
+// console.log(x);
+// x = x + 1;
+// console.log(x);
+
+
+//THESE ARE 3 DIFFERENT WAYS TO DECREASE Y BY 1
+// var y = 8;
+// y -= 1;
+// console.log(y);
+// y--;
+// console.log(y);
+// y = y - 1;
+// console.log(y);
+//MINIFY JS: var y=8;y-=1,console.log(y),y--,console.log(y),y-=1,console.log(y);
+
+// THESE ARE 2 WAYS TO MULTIPLY Z BY 2
+// var z = 8;
+// z = z * 2;
+// console.log(z);
+// z *= 2;
+// console.log(z);
+
+// THESE ARE 2 WAYS TO DIVIDE A BY 2 IN PLACE
+// var a = 8;
+// a = a / 2;
+// console.log(a);
+// a /= 2;
+// console.log(a);
+
+//MODULUS TAKES THE VALUE AND DIVIDES IT BY A NUMBER. THEN IT RETURNS A REMAINDER 
+// var b = 17;
+// b = b % 2;
+// console.log(b);
+
+//MODULUS FUNCTION
+function evenOrOdd(num) {
+	var tester = num % 2;
+	if (tester === 0) {
+		console.log("The value passed is EVEN");
+	} else if (tester ===1) {
+		console.log("The value passed is  ODD")
+	} else {
+		console.log("Please provide a number for this function");
+	}
+}
+
+evenOrOdd(106);
